eeacfe1728dc4019cbb31d8fcfa07343d7a28274
bonnina <37539513+bonnina@users.noreply.github.com>
2020-04-23 09:09:47 +0300

    Add Neuron functions

--- basic_NN_keras.net/Neuron.cs
+++ basic_NN_keras.net/Neuron.cs
@@ -5,6 +5,9 @@ namespace basic_NN_keras.net
 {
     class Neuron
     {
+        // receives the input values, does the weighted sum of all the input signals
+        // from all dendrites and passes them to the activation function
+
         public List<Dendrite> Dendrites { get; set; }
 
         public Pulse OutputPulse { get; set; }
@@ -15,6 +18,39 @@ namespace basic_NN_keras.net
         {
             Dendrites = new List<Dendrite>();
             OutputPulse = new Pulse();
+        }
+
+        public void Fire()
+        {
+            OutputPulse.Value = Sum();
+
+            OutputPulse.Value = Activation(OutputPulse.Value);
+        }
+
+        public void Compute(double learningRate, double delta)
+        {
+            Weight += learningRate * delta;
+            foreach (var terminal in Dendrites)
+            {
+                terminal.SynapticWeight = Weight;
+            }
+        }
+
+        private double Sum()
+        {
+            double computeValue = 0.0f;
+            foreach (var d in Dendrites)
+            {
+                computeValue += d.InputPulse.Value * d.SynapticWeight;
+            }
+
+            return computeValue;
+        }
+
+        private double Activation(double input)
+        {
+            double threshold = 1;
+            return input >= threshold ? 0 : threshold;
         }
     }
 }
