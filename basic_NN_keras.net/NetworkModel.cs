0d8806e369afc751687117d564c5ffd0bebb46e9
bonnina <37539513+bonnina@users.noreply.github.com>
2020-04-26 23:02:48 +0300

    Add NetworkModel class

---
+++ basic_NN_keras.net/NetworkModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+
+namespace basic_NN_keras.net
+{
+    class NetworkModel
+    {
+        public List<NeuralLayer> Layers { get; set; }
+
+        public NetworkModel()
+        {
+            Layers = new List<NeuralLayer>();
+        }
+
+        public void AddLayer(NeuralLayer layer)
+        {
+            int dendriteCount = 1;
+
+            if (Layers.Count > 0)
+            {
+                dendriteCount = Layers.Last().Neurons.Count;
+            }
+
+            foreach (var element in layer.Neurons)
+            {
+                for (int i = 0; i < dendriteCount; i++)
+                {
+                    element.Dendrites.Add(new Dendrite());
+                }
+            }
+        }
+
+        public void Build()
+        {
+            int i = 0;
+            foreach (var layer in Layers)
+            {
+                if (i >= Layers.Count - 1)
+                {
+                    break;
+                }
+
+                var nextLayer = Layers[i + 1];
+                CreateNetwork(layer, nextLayer);
+
+                i++;
+            }
+        }
+
+        private void CreateNetwork(NeuralLayer connectingFrom, NeuralLayer connectingTo)
+        {
+            foreach (var to in connectingTo.Neurons)
+            {
+                foreach (var from in connectingFrom.Neurons)
+                {
+                    to.Dendrites.Add(new Dendrite() { InputPulse = to.OutputPulse, SynapticWeight = connectingTo.Weight });
+                }
+            }
+        }
+    }
+}
