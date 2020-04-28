d90c21b7421be134542d0f462e7cfdb3a4f27189
bonnina <37539513+bonnina@users.noreply.github.com>
2020-04-28 23:28:02 +0300

    Update Main to create a network with three layers with initial weights

--- basic_NN_keras.net/Program.cs
+++ basic_NN_keras.net/Program.cs
@@ -4,9 +4,15 @@ namespace basic_NN_keras.net
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            NetworkModel model = new NetworkModel();
+            model.Layers.Add(new NeuralLayer(2, 0.1, "INPUT"));
+            model.Layers.Add(new NeuralLayer(2, 0.1, "HIDDEN"));
+            model.Layers.Add(new NeuralLayer(1, 0.1, "OUTPUT"));
+
+            model.Build();
+            model.Print();
         }
     }
 }
