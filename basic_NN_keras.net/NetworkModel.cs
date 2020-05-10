b7ffe448dee4a0f2ace4921717e308bf0f592db2
bonnina <37539513+bonnina@users.noreply.github.com>
2020-05-10 23:07:02 +0300

    Add Train method

--- basic_NN_keras.net/NetworkModel.cs
+++ basic_NN_keras.net/NetworkModel.cs
@@ -122,5 +122,49 @@ namespace basic_NN_keras.net
                 layer.Optimize(lr, 1);
             }
         }
+
+        public void Train(NeuralData X, NeuralData Y, int iterations, double learningRate = 0.1)
+        {
+            int epoch = 1;
+            //Loop till the number of iterations
+            while (iterations >= epoch)
+            {
+                //Get the input layers
+                var inputLayer = Layers[0];
+                List<double> outputs = new List<double>();
+
+                //Loop through the record
+                for (int i = 0; i < X.Data.Length; i++)
+                {
+                    //Set the input data into the first layer
+                    for (int j = 0; j < X.Data[i].Length; j++)
+                    {
+                        inputLayer.Neurons[j].OutputPulse.Value = X.Data[i][j];
+                    }
+
+                    //Fire all the neurons and collect the output
+                    ComputeOutput();
+                    outputs.Add(Layers.Last().Neurons.First().OutputPulse.Value);
+                }
+
+                //Check the accuracy score against Y with the actual output
+                double accuracySum = 0;
+                int y_counter = 0;
+                outputs.ForEach((x) =>
+                {
+                    if (x == Y.Data[y_counter].First())
+                    {
+                        accuracySum++;
+                    }
+
+                    y_counter++;
+                });
+
+                //Optimize the synaptic weights
+                OptimizeWeights(accuracySum / y_counter);
+                Console.WriteLine("Epoch: {0}, Accuracy: {1} %", epoch, (accuracySum / y_counter) * 100);
+                epoch++;
+            }
+        }
     }
 }
