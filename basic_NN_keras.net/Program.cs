7ba9ff325c4ceffb3e3dc72764939fbc9fc494d6
bonnina <37539513+bonnina@users.noreply.github.com>
2020-05-14 17:50:29 +0300

    Update main

--- basic_NN_keras.net/Program.cs
+++ basic_NN_keras.net/Program.cs
@@ -12,6 +12,26 @@ namespace basic_NN_keras.net
             model.Layers.Add(new NeuralLayer(1, 0.1, "OUTPUT"));
 
             model.Build();
+            Console.WriteLine("----Before Training------------");
+            model.Print();
+
+            Console.WriteLine();
+
+            NeuralData X = new NeuralData(4);
+            X.Add(0, 0);
+            X.Add(0, 1);
+            X.Add(1, 0);
+            X.Add(1, 1);
+
+            NeuralData Y = new NeuralData(4);
+            Y.Add(0);
+            Y.Add(0);
+            Y.Add(0);
+            Y.Add(1);
+
+            model.Train(X, Y, iterations: 10, learningRate: 0.1);
+            Console.WriteLine();
+            Console.WriteLine("----After Training------------");
             model.Print();
         }
     }
